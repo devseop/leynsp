75b9a602f1c89189e873cd8f755ebf26a5d612fe
devseop <cocc263@gmail.com>
2025-05-19 19:20:06 +0900

    :recycle: Migration

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string().transform((str) => new Date(str)),
+  }),
+});
+
+export const collections = {
+  posts,
+};
