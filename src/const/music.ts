f716d5a27e27e35651ece5d92d69b0f36d3811a4
devseop <cocc263@gmail.com>
2025-07-02 16:06:11 +0900

    :sparkles: Update playlist

--- src/const/music.ts
+++ src/const/music.ts
@@ -10,142 +10,185 @@ interface Music {
 export const musics: Music[] = [
   {
     id: 1,
-    title: '새해 복',
-    artistName: '장기하와 얼굴들',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/4841/484117.jpg?version=undefined',
-    listenedAt: 2025
+    title: "새해 복",
+    artistName: "장기하와 얼굴들",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/4841/484117.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 2,
-    title: 'Von dutch',
-    artistName: 'Charli xcx',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/350790/35079096.jpg?version=undefined',
-    listenedAt: 2025
+    title: "Von dutch",
+    artistName: "Charli xcx",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/350790/35079096.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 3,
-    title: 'Candle Flame',
-    artistName: 'Jungle, Erick the Architect',
-    albumCover: 'https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2Fe0aa437823e7c330d87daf83dbfb1a23.1000x1000x1.png',
-    listenedAt: 2025
+    title: "Candle Flame",
+    artistName: "Jungle, Erick the Architect",
+    albumCover:
+      "https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2Fe0aa437823e7c330d87daf83dbfb1a23.1000x1000x1.png",
+    listenedAt: 2025,
   },
   {
     id: 4,
-    title: 'space',
-    artistName: 'The Poles',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/204454/20445484.jpg?version=undefined',
-    listenedAt: 2025
+    title: "space",
+    artistName: "The Poles",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/204454/20445484.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 5,
-    title: 'Hurry Up Tomorrow',
-    artistName: 'The Weeknd',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/371003/37100348.jpg?version=undefined',
-    listenedAt: 2025
+    title: "Hurry Up Tomorrow",
+    artistName: "The Weeknd",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/371003/37100348.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 6,
-    title: '그 때 그 노래',
-    artistName: '장기하와 얼굴들',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/2894/289433.jpg?version=undefined',
-    listenedAt: 2025
+    title: "그 때 그 노래",
+    artistName: "장기하와 얼굴들",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/2894/289433.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 7,
-    title: 'Pink + White',
-    artistName: 'Frank Ocean',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/191358/19135867.jpg?version=undefined',
-    listenedAt: 2025
+    title: "Pink + White",
+    artistName: "Frank Ocean",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/191358/19135867.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 8,
-    title: 'MUTT',
-    artistName: 'Leon Thomas',
-    albumCover: 'https://t2.genius.com/unsafe/1640x0/https%3A%2F%2Fimages.genius.com%2F768420f09187fab9d0d101ed280a6c7b.592x592x1.png',
-    listenedAt: 2025
+    title: "MUTT",
+    artistName: "Leon Thomas",
+    albumCover:
+      "https://t2.genius.com/unsafe/1640x0/https%3A%2F%2Fimages.genius.com%2F768420f09187fab9d0d101ed280a6c7b.592x592x1.png",
+    listenedAt: 2025,
   },
   {
     id: 9,
-    title: 'The Hardest Button to Button',
-    artistName: 'The White Stripes',
-    albumCover: 'https://t2.genius.com/unsafe/1640x0/https%3A%2F%2Fimages.genius.com%2Fab0bb0fe252cf559f3aed5a635065920.1000x1000x1.jpg',
-    listenedAt: 2025
+    title: "The Hardest Button to Button",
+    artistName: "The White Stripes",
+    albumCover:
+      "https://t2.genius.com/unsafe/1640x0/https%3A%2F%2Fimages.genius.com%2Fab0bb0fe252cf559f3aed5a635065920.1000x1000x1.jpg",
+    listenedAt: 2025,
   },
   {
     id: 10,
-    title: '530',
-    artistName: '¥$, Kanye West & Ty Dolla $ign',
-    albumCover: 'https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2F3d7bdca736daf5a069b4d5e86f5ff086.1000x1000x1.png',
-    listenedAt: 2025
+    title: "530",
+    artistName: "¥$, Kanye West & Ty Dolla $ign",
+    albumCover:
+      "https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2F3d7bdca736daf5a069b4d5e86f5ff086.1000x1000x1.png",
+    listenedAt: 2025,
   },
   {
     id: 11,
-    title: 'If You Love Me',
-    artistName: 'Melody Gardot',
-    albumCover: 'https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2Fc43ee57c9b09a2133feb2658d2d824fc.1000x1000x1.jpg',
-    listenedAt: 2025
+    title: "If You Love Me",
+    artistName: "Melody Gardot",
+    albumCover:
+      "https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2Fc43ee57c9b09a2133feb2658d2d824fc.1000x1000x1.jpg",
+    listenedAt: 2025,
   },
   {
     id: 12,
-    title: 'either on or off the drugs',
-    artistName: 'JPEGMAFIA',
-    albumCover: 'https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2F38f09259fe75c101743d1ddb3ed72ca7.999x999x1.png',
-    listenedAt: 2025
+    title: "either on or off the drugs",
+    artistName: "JPEGMAFIA",
+    albumCover:
+      "https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2F38f09259fe75c101743d1ddb3ed72ca7.999x999x1.png",
+    listenedAt: 2025,
   },
   {
     id: 13,
-    title: '사랑으로',
-    artistName: 'wave to earth',
-    albumCover: 'https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2Fe2b95782993b2c0739c03c61fee06a13.1000x1000x1.png',
-    listenedAt: 2025
+    title: "사랑으로",
+    artistName: "wave to earth",
+    albumCover:
+      "https://t2.genius.com/unsafe/516x516/https%3A%2F%2Fimages.genius.com%2Fe2b95782993b2c0739c03c61fee06a13.1000x1000x1.png",
+    listenedAt: 2025,
   },
   {
     id: 14,
-    title: 'N29 to Berlin - A Colors Show',
-    artistName: 'Ezra Collective',
-    albumCover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/12/8b/a8/128ba8f4-27ee-440c-2e95-cebf5d26fe08/840054904875.png/592x592bb.webp',
-    listenedAt: 2025
+    title: "N29 to Berlin - A Colors Show",
+    artistName: "Ezra Collective",
+    albumCover:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/12/8b/a8/128ba8f4-27ee-440c-2e95-cebf5d26fe08/840054904875.png/592x592bb.webp",
+    listenedAt: 2025,
   },
   {
     id: 15,
-    title: 'Safeandsound',
-    artistName: 'Parcels',
-    albumCover: 'https://t2.genius.com/unsafe/1640x0/https%3A%2F%2Fimages.genius.com%2F3eef8e5656c3c9844b6208a7144fc84d.500x500x1.jpg',
-    listenedAt: 2025
+    title: "Safeandsound",
+    artistName: "Parcels",
+    albumCover:
+      "https://t2.genius.com/unsafe/1640x0/https%3A%2F%2Fimages.genius.com%2F3eef8e5656c3c9844b6208a7144fc84d.500x500x1.jpg",
+    listenedAt: 2025,
   },
   {
     id: 16,
-    title: '이 말을 하고 싶었어요',
-    artistName: '김창완',
-    albumCover: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/8b/76/a6/8b76a6dc-8dc9-812a-e725-278ce2f9b761/1963621194062.jpg/592x592bb.webp',
-    listenedAt: 2025
+    title: "이 말을 하고 싶었어요",
+    artistName: "김창완",
+    albumCover:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/8b/76/a6/8b76a6dc-8dc9-812a-e725-278ce2f9b761/1963621194062.jpg/592x592bb.webp",
+    listenedAt: 2025,
   },
   {
     id: 17,
-    title: 'Last Scene (feat. Wonstein)',
-    artistName: '아이유',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/41204/4120415.jpg?version=undefined',
-    listenedAt: 2025
+    title: "Last Scene (feat. Wonstein)",
+    artistName: "아이유",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/41204/4120415.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 18,
-    title:'Dreamin Of The Past (feat. Kanye West)',
-    artistName: 'Pusha T',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/177196/17719682.jpg?version=undefined',
-    listenedAt: 2025
+    title: "Dreamin Of The Past (feat. Kanye West)",
+    artistName: "Pusha T",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/177196/17719682.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 19,
-    title: 'Mad Funk Camp ALl Starz',
-    artistName: '아소토 유니온',
-    albumCover: 'https://image.bugsm.co.kr/album/images/original/331/33175.jpg?version=undefined',
-    listenedAt: 2025
+    title: "Mad Funk Camp ALl Starz",
+    artistName: "아소토 유니온",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/original/331/33175.jpg?version=undefined",
+    listenedAt: 2025,
   },
   {
     id: 20,
-    albumName: '살아숨셔 4',
-    artistName: '염따',
-    albumCover: 'https://image.bugsm.co.kr/album/images/1000/207324/20732484.jpg',
-    listenedAt: 2025
-  }
-]
+    albumName: "살아숨셔 4",
+    artistName: "염따",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/1000/207324/20732484.jpg",
+    listenedAt: 2025,
+  },
+  {
+    id: 20,
+    title: "Attention (250 Remix)",
+    artistName: "뉴진스",
+    albumCover: "https://image.bugsm.co.kr/album/images/1000/40943/4094363.jpg",
+    listenedAt: 2025,
+  },
+  {
+    id: 21,
+    title: "EKO EKO",
+    artistName: "m-flo loves ZICO, eill",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/1000/207321/20732181.jpg",
+    listenedAt: 2025,
+  },
+  {
+    id: 22,
+    albumName: "야자수",
+    artistName: "리짓군즈",
+    albumCover:
+      "https://image.bugsm.co.kr/album/images/1000/200513/20051384.jpg",
+    listenedAt: 2025,
+  },
+];
