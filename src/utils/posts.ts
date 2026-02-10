0200c020d7013610bf3c73167f2164b58d7d0845
devseop <cocc263@gmail.com>
2026-02-10 23:03:33 +0900

    chore: clear project for tanstack start migration

--- src/utils/posts.ts
+++
@@ -1,9 +0,0 @@
-import { getCollection } from 'astro:content';
-
-export async function getPublishedPosts() {
-  const posts = await getCollection('posts', ({ data }) => {
-    return import.meta.env.PROD ? data.draft !== true : true;
-  });
-  posts.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime());
-  return posts;
-}
