b96700a30ed40cb7df749a8ee87c52f71590d160
devseop <cocc263@gmail.com>
2026-03-31 16:36:00 +0900

    :lipstick: Add 2025 playlist

--- src/soundArchive/data/images.ts
+++ src/soundArchive/data/images.ts
@@ -4,6 +4,46 @@ import sohSohCover from '../../assets/soundArchive/2026/soh_soh.png'
 import vol7Cover from '../../assets/soundArchive/2026/vol7.png'
 import yinAndYangCover from '../../assets/soundArchive/2026/yin_and_yang.png'
 import hereWeAreCover from '../../assets/soundArchive/2025/here_we_are.png'
+import bullyCover from '../../assets/soundArchive/2026/bully.png'
+import lindyEfxCover from '../../assets/soundArchive/2025/lindy_efx.png'
+import karmaCollectorCover from '../../assets/soundArchive/2025/karma_collector.png'
+import stayHereCover from '../../assets/soundArchive/2025/stay_here.png'
+import zeroPlusZeroCover from '../../assets/soundArchive/2025/zero_plus_zero.png'
+import becomePoetryCover from '../../assets/soundArchive/2025/become_poetry.png'
+import sonOfSpergyCover from '../../assets/soundArchive/2025/son_of_spergy.png'
+import brownSugarCover from '../../assets/soundArchive/2025/brown_sugar.png'
+import willBeOkayCover from '../../assets/soundArchive/2025/will_be_okay.png'
+import sitcomCover from '../../assets/soundArchive/2025/sitcom.png'
+import poserCover from '../../assets/soundArchive/2025/poser.png'
+import gosuCover from '../../assets/soundArchive/2025/gosu.png'
+import babyCover from '../../assets/soundArchive/2025/baby.png'
+import iveFoundYouCover from '../../assets/soundArchive/2025/ive_found_you.png'
+import stopTheSeasonCover from '../../assets/soundArchive/2025/stop_the_season.png'
+import vividLalaLoveCover from '../../assets/soundArchive/2025/vivid_lala_love.png'
+import route7Cover from '../../assets/soundArchive/2025/route_7.png'
+import becauseImMeCover from '../../assets/soundArchive/2025/because_im_me.png'
+import palmTreeCover from '../../assets/soundArchive/2025/palm_tree.png'
+import ekoEkoCover from '../../assets/soundArchive/2025/eko_eko.png'
+import attention250Cover from '../../assets/soundArchive/2025/attention_250.png'
+import aliveAndWell4Cover from '../../assets/soundArchive/2025/alive_and_well.png'
+import madFunkCampAllStarzCover from '../../assets/soundArchive/2025/mad_funk_camp_all_starz.png'
+import dreaminOfThePastCover from '../../assets/soundArchive/2025/dreamin_of_the_past.png'
+import lastSceneCover from '../../assets/soundArchive/2025/last_scene.png'
+import wantedToSayCover from '../../assets/soundArchive/2025/wanted_to_say.png'
+import safeAndSoundCover from '../../assets/soundArchive/2025/safe_and_sound.png'
+import n29ToBerlinCover from '../../assets/soundArchive/2025/n29_to_berlin.png'
+import withLoveCover from '../../assets/soundArchive/2025/with_love.png'
+import eitherOnOrOffTheDrugsCover from '../../assets/soundArchive/2025/either_on_or_off_the_drugs.png'
+import ifYouLoveMeCover from '../../assets/soundArchive/2025/if_you_love_me.png'
+import fiftyThreeZeroCover from '../../assets/soundArchive/2025/530.png'
+import theHardestButtonToButtonCover from '../../assets/soundArchive/2025/the_hardest_button_to_button.png'
+import muttCover from '../../assets/soundArchive/2025/mutt.png'
+import pinkWhiteCover from '../../assets/soundArchive/2025/pink_white.png'
+import thatSongCover from '../../assets/soundArchive/2025/that_song.png'
+import hurryUpTomorrowCover from '../../assets/soundArchive/2025/hurry_up_tomorrow.png'
+import spaceCover from '../../assets/soundArchive/2025/space.png'
+import candleFlameCover from '../../assets/soundArchive/2025/candle_flame.png'
+import lunarNewYearCover from '../../assets/soundArchive/2025/lunar_new_year.png'
 
 export const soundArchiveCoverImages = {
   vol07: vol7Cover,
@@ -12,4 +52,44 @@ export const soundArchiveCoverImages = {
   doYourThingBabe: doYourThingBabeCover,
   sohSoh: sohSohCover,
   hereWeAre: hereWeAreCover,
+  bully: bullyCover,
+  lindyEfx: lindyEfxCover,
+  karmaCollector: karmaCollectorCover,
+  stayHere: stayHereCover,  
+  zeroPlusZero: zeroPlusZeroCover,
+  becomePoetry: becomePoetryCover,
+  sonOfSpergy: sonOfSpergyCover,
+  brownSugar: brownSugarCover,
+  willBeOkay: willBeOkayCover,
+  sitcom: sitcomCover,
+  poser: poserCover,
+  gosu: gosuCover,
+  baby: babyCover,
+  iveFoundYou: iveFoundYouCover,
+  stopTheSeason: stopTheSeasonCover,
+  vividLalaLove: vividLalaLoveCover,
+  route7: route7Cover,
+  becauseImMe: becauseImMeCover,
+  palmTree: palmTreeCover,
+  ekoEko: ekoEkoCover,
+  attention250: attention250Cover,
+  aliveAndWell4: aliveAndWell4Cover,
+  madFunkCampAllStarz: madFunkCampAllStarzCover,
+  dreaminOfThePast: dreaminOfThePastCover,
+  lastScene: lastSceneCover,
+  wantedToSay: wantedToSayCover,
+  safeAndSound: safeAndSoundCover,
+  n29ToBerlin: n29ToBerlinCover,
+  withLove: withLoveCover,
+  eitherOnOrOffTheDrugs: eitherOnOrOffTheDrugsCover,
+  ifYouLoveMe: ifYouLoveMeCover,
+  fiftyThreeZero: fiftyThreeZeroCover,
+  theHardestButtonToButton: theHardestButtonToButtonCover,
+  mutt: muttCover,
+  pinkWhite: pinkWhiteCover,
+  thatSong: thatSongCover,
+  hurryUpTomorrow: hurryUpTomorrowCover,
+  space: spaceCover,
+  candleFlame: candleFlameCover,
+  lunarNewYear: lunarNewYearCover,
 } as const
